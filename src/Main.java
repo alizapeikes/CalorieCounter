4125a75dada0c6156453939dc6dce17b35635dad
alizapeikes <79273849+alizapeikes@users.noreply.github.com>
2021-10-24 18:59:06 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	/*
+	 * Create a diet class 
+	 * field - name, max calories, min calories
+	 * 
+	 * Create a person class
+	 * fields - name, height, weight, diet choice 
+	 * 
+	 * create a day class 
+	 * fields - calories tracker, max calorie
+	 * 
+	 * Create an enum of genders
+	 */
+}
